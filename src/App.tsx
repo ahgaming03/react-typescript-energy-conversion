7f032d75552646985af19ae9a5d54f22a1467761
ahgaming03 <ahgamingofficial03@gmail.com>
2024-09-12 21:46:59 +0700

    Initial project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex min-h-[100vh] items-center justify-center text-5xl font-bold">
+        Hello world!
+      </div>
+    </>
+  );
+}
+
+export default App;
